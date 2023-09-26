922c8ced40a8e9d13a342d0b1ede3524efff0f54
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2023-09-26 23:24:08 +0530

    updated chat service

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/style.css';
 
-function chat() {
+function Chat() {
+    const defaultQuestions = [
+        "How can I contact support?",
+        "Tell me more about your services.",
+        "Where can I view my current and past bills on the Sri-Care platform?",
+        "How can I make online payments for my telecommunications services?",
+        "How can I view my payment history?",
+        "What payment methods are accepted for online bill payments?",
+        "How do I set up email notifications for my account?",
+        "What are the customer care agents' working hours for live chat support?"
+    ];
+
+    const [chatMessages, setChatMessages] = useState([
+        { text: "Hello! How can I help you?", className: "message" },
+    ]);
+
+    const handleQuestionClick = (question) => {
+        setChatMessages([...chatMessages, { text: question, className: "message user" }]);
+    };
+
     return (
         <div>
             <h2>Chat</h2>
             <div className="chat-container">
                 <div className="chat-messages">
-                    <div className="message">Hello! How can I help you?</div>
-                    <div className="message user">Hi! I have a question.</div>
-                    <div className="message">Sure, go ahead and ask.</div>
+                    {chatMessages.map((message, index) => (
+                        <div key={index} className={message.className}>
+                            {message.text}
+                        </div>
+                    ))}
                 </div>
-                <br/>
-                <form className="message">
-                    <input type="text" placeholder="Type your message..."/>
-                    <input type="submit" value="Send"/>
-                </form>
+                <br />
+                <hr/>
+                <div className="default-questions">
+                    {defaultQuestions.map((question, index) => (
+                        <button
+                            key={index}
+                            onClick={() => handleQuestionClick(question)}
+                            className="question-button"
+                        >
+                            {question}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
 }
 
-export default chat;
+export default Chat;
