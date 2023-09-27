1b9cdf5ab4b6791d8e1921826d51b96cc6ac965b
Ravindu Wegiriya <ravinduwegiriya@gmail.com>
2023-09-27 18:35:18 +0530

    add card verification to bill payment service

--- micro-services/bill-payment/src/main/java/com/sritel/billpayment/BillPaymentController.java
+++ micro-services/bill-payment/src/main/java/com/sritel/billpayment/BillPaymentController.java
@@ -10,6 +10,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class BillPaymentController {
 
+    private final String sampleCardNumber = "1234567812345678";
+    private final String sampleCardHolderName = "Ravindu Wegiriya";
+    private final String sampleCardExpiry = "12/25";
+    private final String sampleCardCVV = "123";
+
     @PostMapping("/initiate-payment")
     public String initiatePayment(@RequestBody Map<String, String> request) {
         String username = request.get("username");
@@ -17,5 +22,24 @@ public class BillPaymentController {
 
         return "User " + username + " is making a payment of Rs. " + paymentValue;
     }
+
+    @PostMapping("/verify-card")
+    public String verifyCard(@RequestBody Map<String, String> cardDetails) {
+        String cardNumber = cardDetails.get("cardNumber");
+        String cardHolderName = cardDetails.get("cardHolderName");
+        String cardExpiry = cardDetails.get("cardExpiry");
+        String cardCVV = cardDetails.get("cardCVV");
+
+        // Compare the received card details with the sample card
+        if (cardNumber.equals(sampleCardNumber) &&
+                cardHolderName.equals(sampleCardHolderName) &&
+                cardExpiry.equals(sampleCardExpiry) &&
+                cardCVV.equals(sampleCardCVV)) {
+            return "Payment accepted";
+        } else {
+            return "Payment failed";
+        }
+    }
+
 
 }
