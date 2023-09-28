231678346ba3519bc07d00f6e02b4878a7e8a478
Ravindu Wegiriya <ravinduwegiriya@gmail.com>
2023-09-28 18:36:30 +0530

    add authentication microservice

--- micro-services/authentication/src/main/java/com/sritel/authentication/AuthenticationController.java
+++ micro-services/authentication/src/main/java/com/sritel/authentication/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.sritel.authentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -10,9 +11,28 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
-@GetMapping("/test")
-public String Test() {
-    return "Authentication Service";
-}
+    // Predefined set of usernames and passwords
+    private static final Map<String, String> userCredentials = new HashMap<>();
 
+    static {
+        userCredentials.put("user1", "password1");
+        userCredentials.put("user2", "password2");
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthenticationModel authenticationModel) {
+        String username = authenticationModel.getEmail();
+        String password = authenticationModel.getPassword();
+
+        if (username == null || password == null) {
+            return "Login failed. Please provide both username and password.";
+        }
+
+        String storedPassword = userCredentials.get(username);
+        if (storedPassword != null && storedPassword.equals(password)) {
+            return "Login successful for User: " + username;
+        } else {
+            return "Login Failed. Invalid username or password.";
+        }
+    }
 }
