e75b68b16e64fb4e9dcdc30642abc63d7ef34e9e
Varsha Wijethunge <varrave16@gmail.com>
2023-11-04 13:15:56 +0530

    Changes in register

--- micro-services/registration/src/main/java/com/sritel/registartion/RegistrationController.java
+++ micro-services/registration/src/main/java/com/sritel/registartion/RegistrationController.java
@@ -1,12 +1,10 @@
 package com.sritel.registartion;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/register")
 @RequiredArgsConstructor
 public class RegistrationController {
